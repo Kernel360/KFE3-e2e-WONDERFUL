5980bf97a4077cdaf89d03b661f3e378c56a65aa
Hyejin <hyejin.frontend@gmail.com>
2025-06-17 22:45:50 +0900

    fix: global ignores를 사용하여 next.config.ts을 전역적으로 무시 (#12) + lefthook.yml 파일 수정: --no-warn-ignored 플래그 추가

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,10 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from '@repo/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  // Global ignores - 다른 속성이 없으면 전역 무시로 동작
+  {
+    ignores: ['next.config.ts'],
+  },
+  ...nextJsConfig,
+];
