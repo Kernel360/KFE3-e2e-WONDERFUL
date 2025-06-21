b6eff7393502881008752f80e9e87ad7a80155ff
Iny <inyoung@web.de>
2025-06-21 19:04:11 +0900

    feat: badge 컴포넌트 제작 #23

---
+++ apps/web/src/components/ui/badge.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const badgeVariants = cva(
+  'inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-sm font-bold w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  {
+    variants: {
+      variant: {
+        primary:
+          'border-transparent bg-indigo-500 text-primary-foreground focus-visible:ring-primary/20 dark:focus-visible:ring-primary/40',
+        secondary:
+          'border-transparent bg-teal-500 text-primary-foreground [a&]:hover:bg-primary/90',
+        closed:
+          'border-transparent bg-neutral-200 text-secondary-foreground [a&]:hover:bg-secondary/90',
+        tertiary: 'border-transparent bg-rose-500 text-white [a&]:hover:bg-destructive/90',
+        outline: 'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  }
+);
+
+function Badge({
+  className,
+  variant,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'span'> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'span';
+
+  return (
+    <Comp data-slot="badge" className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
