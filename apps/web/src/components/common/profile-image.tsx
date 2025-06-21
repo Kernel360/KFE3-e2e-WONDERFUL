a5f97b2cab64d69a2cd0eb002d01b914cf5c24ae
Iny <inyoung@web.de>
2025-06-21 20:50:47 +0900

    feat: profile image 제작

---
+++ apps/web/src/components/common/profile-image.tsx
@@ -0,0 +1,11 @@
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+
+const ProfileImage = () => {
+  return (
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+    </Avatar>
+  );
+};
+
+export default ProfileImage;
