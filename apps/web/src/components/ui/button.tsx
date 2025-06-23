b8af89d3063b923ef8e0f22cca6fb6d4c64c16bc
젤리 <stay@jelliui-MacBookAir.local>
2025-06-23 18:52:32 +0900

    feat: Button 컴포넌트 제작
    - text
    - text+icon
    - icon

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -6,38 +6,91 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md  transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive cursor-pointer",
+  "inline-flex items-center justify-center gap-1.5 whitespace-nowrap rounded-md transition-all [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
         // defualt=solid type
-        default: '[&_svg]:stroke-white',
-        outline: 'border bg-background  hover:bg-accent hover:text-accent-foreground',
-      },
-      size: {
-        default: 'h-11 px-4 py-2 has-[>svg]:px-3 text-base font-weight-medium',
-        min: 'h-8 text-sm font-weight-semibold px-3',
-        sm: 'h-10 rounded-md gap-1.5 px-4 has-[>svg]:px-2.5 text-sm font-weight-medium ',
-        lg: 'h-12 rounded-md px-5 has-[>svg]:px-4 text-lg',
+        solid: '[&_svg]:!h-auto',
+        outline: 'border-1 bg-transparent',
       },
       color: {
         // defualt=primary color
-        default: 'bg-primary-500 text-primary-foreground hover:bg-primary-700',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        disabled: 'hover:bg-accent hover:text-accent-foreground ',
-        positive: '',
-        negative:
-          'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 ',
+        primary: 'focus-visible:ring-primary-500/50',
+        secondary: 'focus-visible:ring-neutral-400/50',
+        disabled: 'cursor-not-allowed ',
+        positive: 'focus-visible:ring-success-600/40',
+        negative: 'focus-visible:ring-danger-600/40',
+      },
+      size: {
+        min: 'h-8 text-sm font-weight-semibold px-3 [&_svg]:!w-4',
+        sm: 'h-10 rounded-md gap-1.5 px-4 has-[>svg]:px-2.5 text-sm font-weight-medium [&_svg]:!w-4.5',
+        medium:
+          'h-11 px-4 py-2 has-[>svg]:px-3 text-base font-weight-medium [&_svg]:!w-6 [&_svg]:stroke-[2px]',
+        lg: 'h-12 rounded-md px-5 has-[>svg]:px-4 text-lg [&_svg]:!w-6.5 [&_svg]:stroke-[2px]',
       },
       fullWidth: {
         true: 'w-full',
         false: '',
       },
     },
+    compoundVariants: [
+      {
+        variant: 'solid',
+        color: 'primary',
+        className: 'bg-primary-500 hover:bg-primary-700 text-white',
+      },
+      {
+        variant: 'solid',
+        color: 'secondary',
+        className:
+          'bg-neutral-200 hover:bg-neutral-300 text-neutral-800 [&_svg]:stroke-neutral-800',
+      },
+      {
+        variant: 'solid',
+        color: 'disabled',
+        className: 'bg-neutral-100 text-neutral-300 [&_svg]:stroke-neutral-300',
+      },
+      {
+        variant: 'solid',
+        color: 'positive',
+        className: 'bg-success-500 hover:bg-success-600 text-white',
+      },
+      {
+        variant: 'solid',
+        color: 'negative',
+        className: 'bg-danger-600 hover:bg-danger-700 text-white',
+      },
+      {
+        variant: 'outline',
+        color: 'primary',
+        className: 'border-primary-500 text-primary-500 hover:bg-primary-50',
+      },
+      {
+        variant: 'outline',
+        color: 'secondary',
+        className: 'border-neutral-400 hover:bg-neutral-100',
+      },
+      {
+        variant: 'outline',
+        color: 'disabled',
+        className: 'border-netral-200 bg-neutral-50 text-neutral-300',
+      },
+      {
+        variant: 'outline',
+        color: 'positive',
+        className: 'border-success-500 text-success-500 hover:bg-success-50',
+      },
+      {
+        variant: 'outline',
+        color: 'negative',
+        className: 'border-danger-600 text-danger-600 hover:bg-danger-50',
+      },
+    ],
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
-      color: 'default',
+      variant: 'solid',
+      size: 'medium',
+      color: 'primary',
       fullWidth: false,
     },
   }
--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,34 +1,7 @@
 'use client';
 
-import { BellElectric } from 'lucide-react';
-
-import { Button } from '../components/ui/button';
-
 const HomePage = () => {
-  return (
-    <div>
-      {/* <h1>메인 홈 페이지</h1> */}
-      <h3>Solid Primary</h3>
-      <div className="mt-2 flex items-center gap-3">
-        <Button size={'min'}>
-          <BellElectric />
-          Button
-        </Button>
-        <Button size={'sm'}>
-          <BellElectric />
-          Button
-        </Button>
-        <Button>
-          <BellElectric />
-          Button
-        </Button>
-        <Button size={'lg'}>
-          <BellElectric />
-          Button
-        </Button>
-      </div>
-    </div>
-  );
+  return <div>{/* <h1>메인 홈 페이지</h1> */}</div>;
 };
 
 export default HomePage;
