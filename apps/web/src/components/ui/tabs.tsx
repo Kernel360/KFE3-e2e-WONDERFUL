bf84d4eeee93a185ac62d73f82b6bf5933c3c589
전민정 <minj_1203@naver.com>
2025-06-24 18:26:26 +0900

    ui: update style code #78

---
+++ apps/web/src/components/ui/tabs.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+
+import { cn } from '@/lib/utils';
+
+function Tabs({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
+      className={cn('flex w-full flex-col gap-2', className)}
+      {...props}
+    />
+  );
+}
+
+function TabsList({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+        'text-muted-foreground inline-flex h-9 w-full items-center justify-center bg-transparent p-[3px]',
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        'border-b-1 inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 whitespace-nowrap border-neutral-200 py-4 text-sm font-medium text-neutral-500 transition-[color,box-shadow] focus-visible:border-b-2 data-[state=active]:border-b-2 data-[state=active]:border-neutral-600 data-[state=active]:bg-transparent data-[state=active]:text-neutral-600',
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function TabsContent({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn('flex-1 outline-none', className)}
+      {...props}
+    />
+  );
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsContent };
