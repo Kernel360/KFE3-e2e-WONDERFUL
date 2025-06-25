6ecf12a4531e4410b8fee4e6273c64e382c245af
젤리 <stay@jelliui-MacBookAir.local>
2025-06-25 20:30:47 +0900

    feat: AuctionItemCard 컴포넌트 생성
    - BidStatusBadge 컴포넌트 분리
    - Badge 컴포넌트 스타일 수정
    - auction type 생성

---
+++ apps/web/src/components/common/auction-item-card.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Link from 'next/link';
+
+import BadgeBidStatus from '@/components/common/badge-bid-status';
+import Thumbnail from '@/components/common/thumbnail';
+import { Badge } from '@/components/ui/badge';
+
+import useCountdown from '@/hooks/useCountdown';
+
+import { AuctionItemCardProps } from '@/types/auction';
+
+const renderTimeBadge = ({ hours, minutes }: { hours: string; minutes: string }) => {
+  const timeBadgeStyle = hours === '00' && minutes === '00' ? 'closed' : 'tertiary';
+
+  return (
+    <Badge variant={timeBadgeStyle} className="pl-1 font-medium">
+      ⏱️ {`${hours}:${minutes}`}
+    </Badge>
+  );
+};
+
+const AuctionItemCard = ({
+  id,
+  title,
+  status,
+  originalPrice,
+  currentPrice,
+  deadline,
+  thumbnailUrl,
+}: AuctionItemCardProps) => {
+  const { hours, minutes } = useCountdown(new Date(deadline));
+
+  return (
+    <Link href={`/product/${id}`} className="flex w-full items-center justify-between gap-2.5">
+      <div className="relative">
+        <BadgeBidStatus status={status} />
+        <Thumbnail url={thumbnailUrl} alt="test dummy" className="w-26.5 h-26.5" />
+      </div>
+      <div className="felx w-full shrink">
+        <p className="leading-5.5 mb-1.5 line-clamp-2 text-lg font-medium text-neutral-900">
+          {title}
+        </p>
+        <div className="flex justify-between">
+          <div className="w-fll flex flex-col justify-evenly gap-2">
+            <p className="text-sm font-medium leading-5 text-neutral-400">
+              시작가 <span className="line-through">{originalPrice.toLocaleString()}</span>
+            </p>
+            {renderTimeBadge({ hours, minutes })}
+          </div>
+          <div className="w-fll flex shrink flex-col justify-end gap-2 text-right">
+            <p className="leading-5">현재 입찰가</p>
+            <p className="text-h3 text-primary-500 leading-none">
+              <strong className="mr-0.5">{currentPrice.toLocaleString('ko-KR')}</strong>원
+            </p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default AuctionItemCard;
