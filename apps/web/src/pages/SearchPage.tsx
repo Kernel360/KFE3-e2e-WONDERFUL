97c50bd956cec7be60535ded2fe396c81695988f
전민정 <minj_1203@naver.com>
2025-06-28 17:57:45 +0900

    feat: 최근 검색어 컴포넌트 제작 #144

--- apps/web/src/pages/SearchPage.tsx
+++ apps/web/src/pages/SearchPage.tsx
@@ -1,9 +1,26 @@
 import HeaderSearch from '@/components/search/header-search';
+import SearchLog, { Log } from '@/components/search/search-log';
+
+const dummy: Log[] = [
+  {
+    id: '1',
+    content: '라이즈 손수건',
+  },
+  {
+    id: '2',
+    content: '캣휠',
+  },
+  {
+    id: '3',
+    content: '휘낭시에',
+  },
+];
 
 const SearchPage = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-3">
       <HeaderSearch />
+      <SearchLog logs={dummy} />
     </div>
   );
 };
