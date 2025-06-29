0f5dd32c8d67e81e472d9b1ce8c156febbc1767a
젤리 <stay@jelliui-MacBookAir.local>
2025-06-29 23:08:52 +0900

    rename: esLint 오류로 인해 컴포넌트 합침

--- apps/web/src/components/add-auction/images-uploader.tsx
+++ apps/web/src/components/add-auction/images-uploader.tsx
@@ -2,17 +2,29 @@
 
 import { useState } from 'react';
 
+import { ImageUp } from 'lucide-react';
+
 import AddAuctionThumbnail from '@/components/add-auction/add-auction-thumbnail';
-import AttachImageInput from '@/components/add-auction/attach-image-input';
+
+import { onChangeImages } from '@/hooks/common/useOnChangeImages';
 
 import { AttacedAuctionImage } from '@/types/auction';
 
 const ImagesUploader = () => {
-  const [saveImages, setSaveImages] = useState<AttacedAuctionImage[]>([{ url: '' }]);
+  const [saveImages, setSaveImages] = useState<AttacedAuctionImage[]>([]);
 
   return (
     <div>
-      <AttachImageInput />
+      <div className="w-fit">
+        <input type="file" id="attachImages" className="hidden" />
+        <label
+          htmlFor="attachImages"
+          className="w-15 h-15 flex flex-col items-center justify-center gap-0.5 rounded-md border border-neutral-200 text-neutral-600"
+        >
+          <ImageUp />
+          <p className="text-min font-medium">0/8</p>
+        </label>
+      </div>
       {!saveImages
         ? ''
         : saveImages!.map((item, idx) => {
