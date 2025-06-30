6945225397ac794daac6113f98cbb1690450ab9b
Iny.K <inyoung@web.de>
2025-06-30 19:23:08 +0900

    [Fix] 플로팅 버튼 위치 수정

--- apps/web/src/components/common/add-auction-button.tsx
+++ apps/web/src/components/common/add-auction-button.tsx
@@ -16,7 +16,7 @@ const AddAuctionButton = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="absolute bottom-28 right-6 z-50">
       <FloatButton
         onClick={handleClick}
         size="medium"
