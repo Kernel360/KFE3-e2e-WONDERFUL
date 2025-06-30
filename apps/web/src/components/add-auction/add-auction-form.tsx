880e3735e1d514aef040cd05909b114b63fbf473
젤리 <stay@jelliui-MacBookAir.local>
2025-06-30 22:49:54 +0900

    feat:  add-acution form 퍼블리싱 및 컴포넌트 생성 완료

---
+++ apps/web/src/components/add-auction/add-auction-form.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Input, Label, Textarea } from '@/components/ui';
+import CategorySelectBox from '@/components/add-auction/category-select';
+import ImagesUploader from '@/components/add-auction/images-uploader';
+import MinUnitSelectBox from '@/components/add-auction/min-unit-select';
+import Notice from '../common/notice';
+
+const colClass = 'space-y-2 [&_label]:text-base [&_label]:text-neutral-900 [&_label]:font-medium';
+
+const AddAcutionForm = () => {
+  return (
+    <div className="space-y-8">
+      <div className={`${colClass}`}>
+        <Label htmlFor="title">제목</Label>
+        <Input id="title" name="title" placeholder="상품명과 함께 간단한 설명이 있으면 좋아요" />
+      </div>
+
+      <div className={`${colClass}`}>
+        <Label htmlFor="description">상품 설명</Label>
+        <Textarea
+          id="description"
+          name="description"
+          className="h-[110px] resize-none overflow-y-auto"
+          placeholder="상품 설명을 입력해주세요. 구매 전 알아야 할 하자나 특이사항을 남겨주세요."
+        />
+      </div>
+
+      <div className={`${colClass}`}>
+        <Label htmlFor="category_id">상품 카테고리</Label>
+        <CategorySelectBox className="w-full" />
+      </div>
+
+      <div className={`${colClass}`}>
+        <Label htmlFor="images">상품 이미지</Label>
+        <ImagesUploader id="images" />
+      </div>
+
+      <fieldset className="space-y-8">
+        <div className={`${colClass}`}>
+          <Label htmlFor="start_price">경매 시작가</Label>
+          <Input
+            id="start_price"
+            name="start_price"
+            type="number"
+            min={1000}
+            placeholder="최소 경매가는 1,000원 입니다."
+          />
+        </div>
+        <div className={`${colClass}`}>
+          <Label htmlFor="min_bid_unit">최소 입찰 금액</Label>
+          <MinUnitSelectBox className="w-full" />
+          <Notice status="">
+            <li>최소 입찰 금액에 대한 간단한 설명</li>
+          </Notice>
+        </div>
+      </fieldset>
+
+      <div className={`${colClass}`}>
+        <Label htmlFor="end_time">경매 종료시간</Label>
+        <Input
+          id="end_time"
+          name="end_time"
+          type="number"
+          max={99}
+          min={0}
+          placeholder="최대 시간은 경매 시작 후 99시간입니다."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AddAcutionForm;
--- apps/web/src/pages/AddAcutionPage.tsx
+++ apps/web/src/pages/AddAcutionPage.tsx
@@ -1,27 +1,40 @@
 'use client';
 
 import { CircleAlert } from 'lucide-react';
-import AddAcutionForm from '../components/add-auction/add-auction-form';
+import AddAcutionForm from '@/components/add-auction/add-auction-form';
 import Notice from '../components/common/notice';
 import { Button } from '../components/ui';
 
-function AddAuctionPage() {
+const AddAuctionPage = () => {
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // 기본 제출 동작 방지
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    console.log(formData);
+  };
+
   return (
-    <form action={''} className="px-[15px]">
-      <AddAcutionForm />
+    <form onSubmit={submitHandler} className="relative mt-2.5">
+      <section className="px-[15px]">
+        <AddAcutionForm />
+        <Notice status="caution" className="mt-11">
+          <li>
+            <CircleAlert />
+            경매를 등록하시면 등록 즉시 시작됩니다.
+          </li>
+        </Notice>
+      </section>
 
-      <Notice status="caution">
-        <li>
-          <CircleAlert />
-          첫번째 내용
-        </li>
-      </Notice>
-
-      <Button className="w-full" size={'lg'}>
-        등록하기
-      </Button>
+      <section className="backdrop-blur-xs from-white-0 sticky bottom-0 bg-white/70 px-[15px] pb-9 pt-4">
+        {/* 모바일 home indicator 계산 배포 환경에서 확인 필요: pb-[env(safe-area-inset-bottom)] */}
+        <Button className="w-full" size={'lg'}>
+          등록하기
+        </Button>
+      </section>
     </form>
   );
-}
+};
 
 export default AddAuctionPage;
