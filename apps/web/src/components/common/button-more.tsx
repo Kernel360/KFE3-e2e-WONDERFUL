5e95a2ab14efc5e6dd84fc8f9fe9911a31509951
전민정 <minj_1203@naver.com>
2025-07-01 19:19:52 +0900

    feat: popover 컴포넌트 제작 #195

---
+++ apps/web/src/components/common/button-more.tsx
@@ -0,0 +1,34 @@
+import { EllipsisVertical } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui';
+
+interface ButtonMoreProps {
+  items: ButtonMoreItem[];
+}
+
+export interface ButtonMoreItem {
+  title: string;
+  onClick: () => void;
+}
+
+const ButtonMore = ({ items }: ButtonMoreProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <button>
+          <EllipsisVertical />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent className="w-25 py-2">
+        <ul className="space-y-2">
+          {items.map((item) => (
+            <li key={item.title} onClick={item.onClick} className="cursor-pointer text-center">
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default ButtonMore;
