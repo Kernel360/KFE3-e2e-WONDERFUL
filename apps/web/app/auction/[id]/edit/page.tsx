d16713d7cda404ec7633dcabe6291c85cec9d35a
miniseung <liontmdals@gmail.com>
2025-07-04 19:10:05 +0900

    fix:Next.js 15 params 변경(#184)

--- apps/web/app/auction/[id]/edit/page.tsx
+++ apps/web/app/auction/[id]/edit/page.tsx
@@ -1,7 +1,11 @@
-import EditAuctionPage from '@/views/EditAuctionPage';
+import EditAuctionPage from '@/pages/EditAuctionPage';
+import { updateAuction } from '@/lib/actions/auction.action';
 
-const Page = ({ params }: { params: { id: string } }) => {
-  return <EditAuctionPage itemId={params.id} />;
+// const Page = ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  // return <EditAuctionPage itemId={params.id} />;
+  return <EditAuctionPage itemId={id} />;
 };
 
 export default Page;
