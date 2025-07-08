284a9d32b95d1ed597113305d9b1cbc1dd3e0e73
Hyejin <hyejin.frontend@gmail.com>
2025-07-08 20:31:08 +0900

    chore: schema.prisma의 UserNotification 모델 추가 및 마이그레이션 파일 제거 (#225) - Prisma 마이그레이션 히스토리 초기화 - 기존 데이터베이스 데이터 보존 - 스키마 드리프트 문제 해결

--- packages/db/prisma/migrations/20250626130325_add_extended_auction_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "auction_prices" ADD COLUMN     "is_extended_auction" BOOLEAN NOT NULL DEFAULT false;
--- packages/db/prisma/migrations/20250702085418_add_status_default/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `status` on the `auction_items` table. The data in that column could be lost. The data in that column will be cast from `VarChar(50)` to `VarChar(20)`.
-
-*/
--- AlterTable
-ALTER TABLE "auction_items" ALTER COLUMN "status" SET DEFAULT 'ACTIVE',
-ALTER COLUMN "status" SET DATA TYPE VARCHAR(20);
