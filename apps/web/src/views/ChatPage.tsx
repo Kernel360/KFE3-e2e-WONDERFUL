81f43b3478cf50e6c09b360890a1c102e7d2bc24
전민정 <minj_1203@naver.com>
2025-07-11 20:26:27 +0900

    style: 불필요한 코드 제거

--- apps/web/src/views/ChatPage.tsx
+++ apps/web/src/views/ChatPage.tsx
@@ -1,11 +1,9 @@
-import ChatInput from '@/components/chat/chat-input';
 import ChatListItem from '@/components/chat/chat-list-item';
 
 const ChatPage = () => {
   return (
     <div className="flex h-full w-full flex-col">
       <ChatListItem />
-      <ChatInput />
     </div>
   );
 };
