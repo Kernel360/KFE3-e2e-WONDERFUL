005082a0b44b75a1c0f1c2a9c5a1ccaaced35244
전민정 <minj_1203@naver.com>
2025-07-11 20:23:16 +0900

    feat: 채팅 input-form 컴포넌트 제작 #143

--- apps/web/src/views/ChatPage.tsx
+++ apps/web/src/views/ChatPage.tsx
@@ -1,9 +1,11 @@
+import ChatInput from '@/components/chat/chat-input';
 import ChatListItem from '@/components/chat/chat-list-item';
 
 const ChatPage = () => {
   return (
-    <div className="flex h-full w-full">
+    <div className="flex h-full w-full flex-col">
       <ChatListItem />
+      <ChatInput />
     </div>
   );
 };
