5b2198fe8d7bc633a88ad8bceb88a1875e33389c
miniseung <liontmdals@gmail.com>
2025-07-14 18:42:33 +0900

    feat:reset-password 퍼블리싱(#232)

--- apps/web/app/auth/reset-password/page.tsx
+++ apps/web/app/auth/reset-password/page.tsx
@@ -1,6 +1,17 @@
-// app/auth/reset-password/page.tsx
-import ResetPasswordView from '@/components/auth/reset-password/reset-password-view';
+import { Suspense } from 'react';
 
-export default function ResetPasswordPage() {
-  return <ResetPasswordView />;
-}
+import ResetPasswordForm from '@/components/auth/reset-password/reset-password-form';
+
+const Page = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center overflow-hidden bg-white px-[154px] pt-[131px]">
+      <div className="mt-[80px] flex w-full flex-col items-center">
+        <Suspense fallback={<div>Loading...</div>}>
+          <ResetPasswordForm />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
