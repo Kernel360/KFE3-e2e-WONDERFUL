0b3a792242c813885ac95f2efb6af283439bdabd
Iny.K <inyoung@web.de>
2025-07-14 19:42:52 +0900

    [refactor] 프로필 수정 페이지 컴포넌트 분리 및 라우트 구조 변경

---
+++ apps/web/app/profile/edit/page.tsx
@@ -0,0 +1,9 @@
+import ProfileEditForm from '@/components/common/profile/edit/form-edit';
+
+export default function ProfileEditPage() {
+  return (
+    <div className="mx-auto flex min-h-screen w-full max-w-md flex-col bg-white">
+      <ProfileEditForm />
+    </div>
+  );
+}
---
+++ apps/web/src/components/common/profile/edit/form-edit.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+import ImageUploader from './image-uploader';
+import NicknameInput from './nickname';
+
+export default function ProfileEditForm() {
+  return (
+    <form className="flex flex-1 flex-col">
+      <ImageUploader />
+      <NicknameInput />
+      <div className="mb-8 mt-auto px-4">
+        <Button type="submit" fullWidth>
+          수정 완료
+        </Button>
+      </div>
+    </form>
+  );
+}
