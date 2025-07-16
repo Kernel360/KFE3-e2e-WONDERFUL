96cfc935b42112a11630648e7e03f72712c9bee2
젤리 <1gyn093@gmail.com>
2025-07-17 01:36:17 +0900

    fix:  입찰자가 없을 경우 스타일 추가

--- apps/web/src/components/auction-detail/bid-table/bid-table.tsx
+++ apps/web/src/components/auction-detail/bid-table/bid-table.tsx
@@ -14,23 +14,30 @@ const BidTable = () => {
   const [animationParent] = useAutoAnimate();
 
   const BIDS = BIDDING_DUMMY.bids;
+
   const sortBid: BidType[] = useMemo(() => {
     return [...BIDS].sort((a, b) => Number(b.price) - Number(a.price)).slice(0, 5);
   }, [BIDS]);
 
   return (
     <div className="bg-primary-50/60 rounded-sm p-3 [&_p]:flex-1">
-      <BidTableHead />
-      <div className="relative">
-        <span className="w-7.5 absolute flex h-full items-center justify-center">
-          <i className="bg-primary-100 h-9/10 block w-1"></i>
-        </span>
-        <ul ref={animationParent} className="space-y-2">
-          {sortBid.map((item) => {
-            return <BidTableRow key={item.bid_id} item={item} />;
-          })}
-        </ul>
-      </div>
+      {BIDS.length < 1 ? (
+        <p className="py-8 text-center">아직 입찰 내역이 없습니다.</p>
+      ) : (
+        <>
+          <BidTableHead />
+          <div className="relative">
+            <span className="w-7.5 absolute flex h-full items-center justify-center">
+              <i className="bg-primary-100 h-9/10 block w-1"></i>
+            </span>
+            <ul ref={animationParent} className="space-y-2">
+              {sortBid.map((item) => {
+                return <BidTableRow key={item.bid_id} item={item} />;
+              })}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 };
