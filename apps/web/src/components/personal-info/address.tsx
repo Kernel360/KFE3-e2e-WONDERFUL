8172db7906531a350f41fd33d17d56c3be2d1707
전민정 <minj_1203@naver.com>
2025-07-17 19:35:57 +0900

    feat: 주소 정보 컴포넌트 제작 #276

---
+++ apps/web/src/components/personal-info/address.tsx
@@ -0,0 +1,46 @@
+import { tv } from 'tailwind-variants';
+
+import { BadgePrimary } from '@/components/personal-info';
+
+const style = tv({
+  base: 'flex items-center justify-between rounded-xl p-3',
+  variants: {
+    color: {
+      default: 'bg-white',
+      selected: 'bg-primary-50/70',
+    },
+  },
+});
+
+interface AddressProps {
+  color?: 'default' | 'selected';
+  children: React.ReactNode;
+  address: Address;
+}
+
+export interface Address {
+  name: string;
+  address: string;
+  phone: string;
+  isPrimary: boolean;
+}
+
+const Address = ({ color, children, address }: AddressProps) => {
+  return (
+    <div className={style({ color })}>
+      <div className="flex-col">
+        <div className="flex items-center gap-2">
+          <p className="text-lg font-medium text-neutral-900">{address.name}</p>
+          {address.isPrimary && <BadgePrimary />}
+        </div>
+        <div className="text-sm font-medium">
+          <p className="text-neutral-900">{address.address}</p>
+          <p className="text-neutral-500">{address.phone}</p>
+        </div>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default Address;
---
+++ apps/web/src/components/personal-info/button-select.tsx
@@ -0,0 +1,19 @@
+import { Check } from 'lucide-react';
+
+import { Button } from '@/components/ui';
+
+interface ButtonSelectProps {
+  isSelected: boolean;
+}
+
+const ButtonSelect = ({ isSelected }: ButtonSelectProps) => {
+  const textColor = isSelected ? 'text-primary-500' : 'text-neutral-400';
+
+  return (
+    <Button variant="solid" color="transparent" size="sm" aria-pressed={isSelected}>
+      <Check className={textColor} strokeWidth={2.5} />
+    </Button>
+  );
+};
+
+export default ButtonSelect;
