b2730d317d0d30d8b97f416fb82b14af7383d518
전민정 <minj_1203@naver.com>
2025-07-18 02:31:58 +0900

    feat: 이미지 송수신 시 컴포넌트 스타일 추가 #291

--- apps/web/src/components/chat/messages/sent-message.tsx
+++ apps/web/src/components/chat/messages/sent-message.tsx
@@ -4,22 +4,25 @@ import { useState } from 'react';
 
 import { ChatBubble } from '@/components/chat';
 
-import { DONE_MESSAGE } from '@/constants/chat';
+import { dummyUrls } from '@/constants/dummy-urls';
 
 const SentMessage = () => {
   // TODO: 실시간 데이터 연동해서 상태값에 따라 bubble 달라질 수 있도록 수정하기
   const [isDone, setIsDone] = useState(false);
+
+  const type = 'image';
   const color = isDone ? 'disabled' : 'primary';
-  const content = isDone
-    ? DONE_MESSAGE
-    : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.';
+  const content = dummyUrls[0]!;
+  // const content = isDone
+  //   ? DONE_MESSAGE
+  //   : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.';
 
   const time = '12:00 AM';
 
   return (
     <div className="flex justify-end gap-2 py-2">
       <p className="flex items-end text-xs font-light text-neutral-500">{time}</p>
-      <ChatBubble color={color} content={content} />
+      <ChatBubble type={type} color={color} content={content} />
     </div>
   );
 };
