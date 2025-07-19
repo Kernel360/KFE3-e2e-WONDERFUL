eff1e4700cb4507248c846e6eb05455999429448
전민정 <minj_1203@naver.com>
2025-07-19 20:30:55 +0900

    feat: 버튼 컴포넌트 분리 및 리팩토링

--- apps/web/src/components/personal-info/button/button-box.tsx
+++ apps/web/src/components/personal-info/button/button-box.tsx
@@ -1,21 +1,32 @@
 'use client';
 
+import Link from 'next/link';
+
 import { Pencil, Trash2 } from 'lucide-react';
 
 import { Button } from '@/components/ui';
 
-const ButtonBox = () => {
-  // TODO: full-page 모달 연결
-  const handleEdit = () => {};
+interface ButtonBoxProps {
+  url: string;
+}
+
+const ButtonBox = ({ url }: ButtonBoxProps) => {
+  // TODO: delete api 요청 함수 호출
   const handleDelete = () => {};
 
   return (
     <div className="flex">
-      <Button variant="solid" color="transparent" size="sm" onClick={handleEdit}>
-        <Pencil className="text-neutral-600" />
-      </Button>
-      <Button variant="solid" color="transparent" size="sm" onClick={handleDelete}>
-        <Trash2 className="text-neutral-600" />
+      <Link href={url} className="flex size-10 items-center justify-center">
+        <Pencil size={20} className="hover:text-primary-500 text-neutral-600" />
+      </Link>
+      <Button
+        variant="solid"
+        color="transparent"
+        size="sm"
+        onClick={handleDelete}
+        className="hover:[&_svg]:text-danger-500"
+      >
+        <Trash2 size={20} className="text-neutral-600" />
       </Button>
     </div>
   );
