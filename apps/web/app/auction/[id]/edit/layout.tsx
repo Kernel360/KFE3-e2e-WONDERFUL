a4ee1694bba387a0364a8200d2aee130d68607c4
Hyejin <hyejin.frontend@gmail.com>
2025-07-19 20:44:57 +0900

    style: 주석 삭제 및 코드 정리

--- apps/web/app/auction/[id]/edit/layout.tsx
+++ apps/web/app/auction/[id]/edit/layout.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 // export default function EditLayout({
-export default async function EditLayout({
+const EditLayout = async ({
   children,
   params,
 }: {
   children: React.ReactNode;
   // params: { id: string };
   params: Promise<{ id: string }>; //Promise로 변경
-}) {
-  const { id } = await params; //await 추가
-  return <>{children}</>;
-}
+}) => {
+  const { id } = await params;
+  // console.log('EditLayout params:', id); // 디버깅용 로그
+  return (
+    <>
+      {children}
+      <div>Edit Layout for Auction ID: {id}</div>
+    </>
+  );
+};
+
+export default EditLayout;
