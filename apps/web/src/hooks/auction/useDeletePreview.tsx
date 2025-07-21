a13308bd825df51c5dd4f2a63028de4c288a94b0
Hyejin <hyejin.frontend@gmail.com>
2025-07-21 23:25:21 +0900

    fix: 이미지를 삭제 후에 setFiles에서 파일이 제대로 제거되지 않아, 삭제된 이미지까지 포함해서 스토리지에 업로드되는 문제 해결 파일 배열에서도 해당 인덱스의 파일을 실제로 제거하므로, 프리뷰와 실제 파일 배열이 동기화 됨

--- apps/web/src/hooks/auction/useDeletePreview.tsx
+++ apps/web/src/hooks/auction/useDeletePreview.tsx
@@ -13,7 +13,13 @@ const deletePreviewImage = ({
   setFiles,
   index,
 }: DeletePreviewImageProps) => {
+  // 프리뷰 이미지에서 해당 인덱스 제거
   setPreviewImages((prev) => prev.filter((_, i) => i !== index));
+
+  // 파일 배열에서도 해당 인덱스 제거
+  setFiles((prev) => prev.filter((_, i) => i !== index));
+
+  // 이미지 개수 감소
   setImgLength((prev) => prev - 1);
 };
 
