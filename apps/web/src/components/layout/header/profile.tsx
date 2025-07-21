709bfe380863e60777a736c0da02613b094d1631
miniseung <liontmdals@gmail.com>
2025-07-22 00:39:22 +0900

    test:프로필페이지에서 위치 모달 테스트용(추후 삭제예정)(#329)

--- apps/web/src/components/layout/header/profile.tsx
+++ apps/web/src/components/layout/header/profile.tsx
@@ -6,15 +6,23 @@ import { Settings } from 'lucide-react';
 
 import { HeaderWrapper } from '@/components/layout/';
 
+//위치 모달 테스트용 추후에 삭제
+import { useLocationModalStore } from '@/lib/zustand/store/location-modal-store';
+
 const ProfileHeader = () => {
   const pathname = usePathname();
-
+  //위치 모달 호출(test) 추후에 삭제할예정
+  const handleClick = () => {
+    const { openLocationModal } = useLocationModalStore.getState();
+    openLocationModal();
+  };
   return (
     <HeaderWrapper className={'bg-white'}>
       {pathname === '/profile' && (
         <>
           <h2 className="text-h4 font-bold">나의 프로필</h2>
-          <Settings />
+          {/* 위치 모달 클릭 이벤트 */}
+          <Settings onClick={handleClick} />
         </>
       )}
     </HeaderWrapper>
