855361ae975e2e00234170aaeba1c0e314215a1b
젤리 <1gyn093@gmail.com>
2025-07-23 00:26:15 +0900

    feature: data 연결, 마지막 메세지 전송 시간 계산 함수 작성

--- apps/web/src/components/chat/chat-list-card.tsx
+++ apps/web/src/components/chat/chat-list-card.tsx
@@ -7,19 +7,14 @@ import { EllipsisVertical } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Thumbnail from '@/components/ui/thumbnail';
 
-import { PREVIEWCHATS } from '@/lib/constants/chat';
-import { dummyUrls } from '@/lib/constants/dummy-urls';
-
 import { ChatRoom } from '@/types/chat';
 
 const ChatListCard = ({ chatInfo }: { chatInfo: ChatRoom }) => {
-  const lastMessageTime = PREVIEWCHATS[9]!.last_message_at;
-  console.log(chatInfo);
-
   const { auction, otherUser, lastMessageAt, messages } = chatInfo;
 
   const msgElapsed = () => {
-    const lastTime = new Date(lastMessageTime).getTime();
+    if (!lastMessageAt) return '';
+    const lastTime = new Date(lastMessageAt).getTime();
     const now = Date.now();
     const gapMs = now - lastTime;
 
@@ -27,48 +22,61 @@ const ChatListCard = ({ chatInfo }: { chatInfo: ChatRoom }) => {
     const oneHour = 60 * oneMinute;
     const oneDay = 24 * oneHour;
     const oneWeek = 7 * oneDay;
-    const twoWeek = 14 * oneDay;
-    const threeWeek = 21 * oneDay;
     const oneMonth = 28 * oneDay;
+    const oneYear = 365 * oneDay;
 
-    if (gapMs < oneHour) {
+    if (gapMs < oneMinute) {
       // 방금
+      return '방금';
+    } else if (gapMs < oneHour) {
       // n분 전
       const minutes = Math.floor(gapMs / oneMinute);
-      return `${minutes}min`;
+      return `${minutes}분 전`;
     } else if (gapMs < oneDay) {
-      //1시간 전
+      // 1시간 전, n시간 전
       const hours = Math.floor(gapMs / oneHour);
-      return `${hours}${hours === 1 ? 'hour' : 'hours'}`;
+      return `${hours}시간 전`;
     } else if (gapMs < oneWeek) {
-      //n일 전
+      // n일 전
       const days = Math.floor(gapMs / oneDay);
-      return `${days}${days === 1 ? 'day' : 'days'}`;
-    } else {
+      return `${days}일 전`;
+    } else if (gapMs < oneMonth) {
+      // n주 전
+      const weeks = Math.floor(gapMs / oneWeek);
+      return `${weeks}주 전`;
+    } else if (gapMs < oneYear) {
       // n달 전
+      const months = Math.floor(gapMs / oneMonth);
+      return `${months}달 전`;
+    } else {
       // n년 전
-      return '일주일 초과';
+      const years = Math.floor(gapMs / oneYear);
+      return `${years}년 전`;
     }
   };
 
-  // const test = msgElapsed();
-
   // TODO: 기능 연결 시 roomId 로 연결 해서 해당 태그 내에서 작성할 것.
-  const roomId = '123';
+  const roomId = chatInfo.id;
   const link = `/chat/${roomId}`;
 
   return (
     <div className="flex h-fit w-full items-center justify-between gap-2.5">
       <Link href={link} className="flex w-full shrink items-center gap-2.5 py-1">
-        <Thumbnail url={dummyUrls[0]!} alt={'sample'} className="w-15 h-15 shrink-0" />
+        <Thumbnail
+          url={auction.thumbnailUrl as string}
+          alt={'sample'}
+          className="w-15 h-15 shrink-0"
+        />
         <div className="flex w-full shrink flex-col">
-          <div className="">
+          <div>
             <p className="flex gap-2">
-              <strong>{PREVIEWCHATS[0]!.opponent_nickname}</strong>
-              <span>{PREVIEWCHATS[0]!.last_message_at}</span>
+              <strong>{otherUser.nickname}</strong>
+              <span>{msgElapsed()}</span>
             </p>
           </div>
-          <p className="">{PREVIEWCHATS[0]!.last_message}</p>
+          <p className="line-clamp-1 min-h-6 flex-1">
+            {!messages.length ? '' : '마지막 메세지 보여주기'}
+          </p>
         </div>
       </Link>
       <Button color="transparent" className="h-10 w-10">
