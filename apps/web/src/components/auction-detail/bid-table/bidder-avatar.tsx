787c46092f17153900dfeb5e58908f47ddc0d19f
젤리 <1gyn093@gmail.com>
2025-08-02 17:11:16 +0900

    style: 주석 삭제

--- apps/web/src/components/auction-detail/bid-table/bidder-avatar.tsx
+++ apps/web/src/components/auction-detail/bid-table/bidder-avatar.tsx
@@ -32,15 +32,3 @@ const BidderAvatar = ({
 };
 
 export default BidderAvatar;
-
-// return (
-//   <>
-//     {userId && (
-//       // 유저 아이디가 있을 때만 아바타 표시
-//       // 프로필 이미지가 없으면 기본 아바타 이미지 사용
-//       <Avatar className="border-primary-500 shadow-primary-200 size-8 border-2 bg-white shadow-md">
-//         <AvatarImage src={profileImg || '/avatar-female.svg'} alt={nickname} />
-//       </Avatar>
-//     )}
-//   </>
-// );
