4cbd8f99888f0ed45b40fb77cdb8585f130e3172
젤리 <1gyn093@gmail.com>
2025-08-03 17:50:12 +0900

    disign: nav 없을 경우 높이값 수정

--- apps/web/src/components/layout/container.tsx
+++ apps/web/src/components/layout/container.tsx
@@ -10,11 +10,11 @@ const Container = ({
   noNav?: boolean;
 }) => {
   const containerStyle = tv({
-    base: 'w-full flex-1 h-full scrollbar-hide-y `',
+    base: 'w-full flex-1 h-full scrollbar-hide-y',
     variants: {
       noNav: {
         true: 'max-h-[calc(100vh-56px)]',
-        false: 'min-h-[calc(100vh-136px)]',
+        false: 'max-h-[calc(100vh)]',
       },
     },
     defaultVariants: {
