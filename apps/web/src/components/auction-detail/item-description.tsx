78997cad25254f60949396a71d5a541f0c720b63
전민정 <minj_1203@naver.com>
2025-08-04 21:26:58 +0900

    fix: 줄바꿈 안되는 컴포넌트 개선

--- apps/web/src/components/auction-detail/item-description.tsx
+++ apps/web/src/components/auction-detail/item-description.tsx
@@ -8,7 +8,7 @@ const ItemDescription = ({ item }: ItemInformationProps) => {
   return (
     <section className="py-6">
       <h3 className="mb-2.5 text-base font-bold">상품 설명</h3>
-      <pre className="whitespace-pre-wrap">{item.description}</pre>
+      <div className="whitespace-pre-wrap">{item.description}</div>
     </section>
   );
 };
