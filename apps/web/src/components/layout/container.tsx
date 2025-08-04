7c85d52cf7bcb7e53aa87ee7b95c90a2e8b9a437
젤리 <1gyn093@gmail.com>
2025-08-04 21:46:17 +0900

    refactor: layout grid 타입으로 수정

--- apps/web/src/components/layout/container.tsx
+++ apps/web/src/components/layout/container.tsx
@@ -1,27 +1,17 @@
-import { tv } from 'tailwind-variants';
-
 const Container = ({
   children,
   className = '',
-  noNav = false,
 }: {
   children: React.ReactNode;
   className?: string;
-  noNav?: boolean;
 }) => {
-  const containerStyle = tv({
-    base: 'w-full flex-1 h-full scrollbar-hide-y',
-    variants: {
-      noNav: {
-        true: 'max-h-[calc(100vh-56px)]',
-        false: 'max-h-[calc(100vh)]',
-      },
-    },
-    defaultVariants: {
-      noNav: false,
-    },
-  });
-  return <main className={`${containerStyle({ noNav: noNav })} ${className}`}>{children}</main>;
+  return (
+    <main
+      className={`scrollbar-hide-y scroll-touch h-full w-full overflow-y-auto overscroll-contain ${className}`}
+    >
+      {children}
+    </main>
+  );
 };
 
 export default Container;
